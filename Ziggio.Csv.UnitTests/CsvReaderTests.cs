53789a290f2f661ab927d7c9db929251dfe341cc
Zach Hlavinka <zhlavinka@gmail.com>
2023-06-29 22:11:40 -0700

    Add more test coverage for CsvReader

--- Ziggio.Csv.UnitTests/CsvReaderTests.cs
+++ Ziggio.Csv.UnitTests/CsvReaderTests.cs
@@ -25,9 +25,24 @@ public class CsvReaderTests {
   }
 
   [TestMethod]
+  [DataRow(TestConstants.TestFiles.MeasureUnitWithHeaders, "\"1000\",\"cup\"")]
+  [DataRow(TestConstants.TestFiles.Food, "\"1105904\",\"branded_food\",\"WESSON Vegetable Oil 1 GAL\",\"\",\"2020-11-13\"")]
+  public void CsvReader_GetLine_ReturnsExpectedLine(string file, string expectedLine) {
+    var fileInfo = TestConstants.TestFiles.GetFileInfo(file);
+
+    using var fileStream = fileInfo.OpenRead();
+    using var streamReader = new StreamReader(fileStream);
+    using var csvReader = new CsvReader(_mockConfiguration.Object, streamReader);
+
+    var line = csvReader.GetLine();
+
+    line.Should().BeEquivalentTo(expectedLine);
+  }
+
+  [TestMethod]
   [DataRow(TestConstants.TestFiles.MeasureUnitWithHeaders, "1000", "cup")]
   [DataRow(TestConstants.TestFiles.Food, "1105904", "branded_food", "WESSON Vegetable Oil 1 GAL", "", "2020-11-13")]
-  public void CsvReader_FirstGetValues_ReturnsStringArray(string file, params string[] expectedValues) {
+  public void CsvReader_GetValues_ReturnsStringArray(string file, params string[] expectedValues) {
     var fileInfo = TestConstants.TestFiles.GetFileInfo(file);
 
     using var fileStream = fileInfo.OpenRead();
