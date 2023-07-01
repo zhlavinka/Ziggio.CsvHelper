8d0c2ab85fbf1a38b5a8e69c4ef1c09196362c1e
Zach Hlavinka <zhlavinka@gmail.com>
2023-06-30 22:01:50 -0700

    Add one more generic csv reader test

--- Ziggio.Csv.UnitTests/GenericCsvReaderTests.cs
+++ Ziggio.Csv.UnitTests/GenericCsvReaderTests.cs
@@ -26,42 +26,68 @@ public class GenericCsvReaderTests {
   }
 
   [TestMethod]
-  [DataRow(TestConstants.TestFiles.MeasureUnitWithHeaders)]
-  public void CsvReader_FirstGetRecord_ReturnsInstance(string file) {
+  [DataRow(TestConstants.TestFiles.FoodShort)]
+  public void CsvReader_GetRecord_ReturnsInstance(string file) {
     var fileInfo = TestConstants.TestFiles.GetFileInfo(file);
 
     using var fileStream = fileInfo.OpenRead();
     using var streamReader = new StreamReader(fileStream);
-    using var csvReader = new CsvReader<MeasureUnit>(_mockConfiguration.Object, streamReader);
+    using var csvReader = new CsvReader<FoodName>(_mockConfiguration.Object, streamReader);
 
-    var measureUnit = csvReader.GetRecord();
+    var food = csvReader.GetRecord();
 
-    measureUnit.Should().NotBeNull();
-    measureUnit.MeasureUnitId.Should().Be(1000);
-    measureUnit.Name.Should().BeEquivalentTo("cup");
-    measureUnit.Abbreviation.Should().BeEquivalentTo(default);
+    food.Should().NotBeNull();
+    food.FdcId.Should().Be(1105904);
+    food.DataType.Should().BeEquivalentTo("branded_food");
+    food.Description.Should().BeEquivalentTo("WESSON Vegetable Oil 1 GAL");
+    food.PublicationDate.Should().Be(new DateOnly(2020, 11, 13));
   }
 
   [TestMethod]
-  [DataRow(TestConstants.TestFiles.MeasureUnitWithHeaders)]
-  public void CsvReader_ReadAllRecords(string file) {
+  [DataRow(TestConstants.TestFiles.FoodShort)]
+  public void CsvReader_GetRecord_LoopAll(string file) {
     var fileInfo = TestConstants.TestFiles.GetFileInfo(file);
 
     using var fileStream = fileInfo.OpenRead();
     using var streamReader = new StreamReader(fileStream);
-    using var csvReader = new CsvReader<MeasureUnit>(_mockConfiguration.Object, streamReader);
+    using var csvReader = new CsvReader<FoodName>(_mockConfiguration.Object, streamReader);
+
+    FoodName food;
+    int numFoods = 0;
+
+    do {
+      food = csvReader.GetRecord();
+
+      if (food is not null)
+        numFoods += 1;
+    }
+    while (food is not null);
+
+    // 38 valid records in file, 2 bad
+    numFoods.Should().Be(38);
+  }
+
+  [TestMethod]
+  [DataRow(TestConstants.TestFiles.FoodShort)]
+  public void CsvReader_GetRecords_ReadsAll(string file) {
+    var fileInfo = TestConstants.TestFiles.GetFileInfo(file);
+
+    using var fileStream = fileInfo.OpenRead();
+    using var streamReader = new StreamReader(fileStream);
+    using var csvReader = new CsvReader<FoodName>(_mockConfiguration.Object, streamReader);
 
     var records = csvReader.GetRecords();
 
     records.Should().NotBeNullOrEmpty();
-    records.Count.Should().Be(122);
+    records.Count.Should().Be(38);
 
-    //"1118","Tablespoons"
-    var spotCheck = records[118];
+    //"388487","branded_food","RASPBERRY DELIGHT","","2019-04-01"
+    var spotCheck = records[19];
 
     spotCheck.Should().NotBeNull();
-    spotCheck.MeasureUnitId.Should().Be(1118);
-    spotCheck.Name.Should().BeEquivalentTo("Tablespoons");
-    spotCheck.Abbreviation.Should().BeEquivalentTo(default);
+    spotCheck.FdcId.Should().Be(388487);
+    spotCheck.DataType.Should().BeEquivalentTo("branded_food");
+    spotCheck.Description.Should().BeEquivalentTo("RASPBERRY DELIGHT");
+    spotCheck.PublicationDate.Should().Be(new DateOnly(2019, 04, 01));
   }
 }
