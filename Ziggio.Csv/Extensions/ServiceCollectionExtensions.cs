bcbd909fcda293435822f3f2319eac84bbeda937
Zach Hlavinka <zhlavinka@gmail.com>
2023-07-01 12:30:07 -0700

    Rename misnamed file to match class name. Add ServiceCollection extension to add required classes to DI

---
+++ Ziggio.Csv/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Ziggio.Csv.Extensions;
+
+public static class ServiceCollectionExtensions {
+  public static IServiceCollection AddZiggioCsv(this IServiceCollection services) {
+    services.TryAddScoped<ICsvConfiguration, CsvConfiguration>();
+    services.TryAddScoped<ICsvParser, CsvParser>();
+    services.TryAddScoped<ICsvReader, CsvReader>();
+
+    return services;
+  }
+}
